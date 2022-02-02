ce5d615890a28f73730a82e78a42951c0515de9f
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-02-02 20:37:06 +0100

    Build Data Model for User, Product and Order.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
@@ -26,6 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function () {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   );
 });
