9f5f9a4928298e19598c2c2a460a46c3d261ea14
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-02-06 08:44:17 +0100

    QTY Select and Add To Cart Button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,18 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
     <Router>
       <Header />
       <main className="py-3">
-      <Container>
-        <Route path="/" component={HomeScreen} exact />
-        <Route path="/product/:id" component={ProductScreen} />
-      </Container>
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+        </Container>
       </main>
       <Footer />
     </Router>
