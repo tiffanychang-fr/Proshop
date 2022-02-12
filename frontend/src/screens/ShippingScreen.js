e62d329480b3d1a2cc06960fdb8db5c3d38adca2
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-02-12 08:58:55 +0100

    frontend checkout process: Create ShippingScreen, PaymentScreen, and PlaceOrderScreen.

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, Form } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps.js";
 import { saveShippingAddress } from "../actions/cartActions";
 
 const ShippingScreen = ({ history }) => {
@@ -22,6 +23,7 @@ const ShippingScreen = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
