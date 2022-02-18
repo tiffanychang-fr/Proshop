7627f1ca9b9bb6ed63dba861c83330a05dd3ff62
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-02-18 11:20:16 +0100

    admin product list

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -50,7 +50,7 @@ function Header() {
                   <LinkContainer to="/admin/userlist">
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to="/admin/userlist">
+                  <LinkContainer to="/admin/productlist">
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
                   <LinkContainer to="/admin/orderlist">
